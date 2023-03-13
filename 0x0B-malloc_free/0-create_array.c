02aec97a5508f4b85772ba673668e022d465d5e8
aquimo <aquimoadriano@gmail.com>
2023-03-13 16:09:31 +0200

    Toos

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"	
 #include <stdlib.h>
+
 /**
  * create_array - create array of size size and assign char c
  * @size: size of array
@@ -8,10 +9,10 @@
  * Return: pointer to array, NULL if fail
  *
  */
+ 
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-
+	char *str;	
 	unsigned int i;
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,7 @@
 #include <stdio.h>		
 #include <stdlib.h>		
-#include "main.h"	
+#include "main.h"
+
 /**	
  * free_grid - frees 2d array	
  * @grid: 2d grid	
@@ -9,19 +10,13 @@
  * Return: nothing	
  *	
  */
-		
+
 void free_grid(int **grid, int height)
-		
 {
-		
 	int i;		
 	for (i = 0; i < height; i++)	
 	{
-		
 		free(grid[i]);
-		
 	}
-		
 	free(grid);
-		
 }
