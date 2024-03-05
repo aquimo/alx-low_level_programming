c54eaf2e25ce7857f68503b36f7d53e54feadb78
aquimo <aquimoadriano@gmail.com>
2024-03-05 11:51:21 +0200

    A

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _islower - checks for lowercase character
+ * _islower - Checks for Lowercase character
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
